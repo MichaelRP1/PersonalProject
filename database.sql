7cc4f0fbc21f96a78e8ff08aadfedb2906e75128
MichaelRP1 <michaelvincastiglia@gmail.com>
2019-07-28 13:50:56 -0400

    Changing Lines in SQL File

--- database.sql
+++ database.sql
@@ -1,22 +1,6 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.40-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- Dumping database structure for findavet
-CREATE DATABASE IF NOT EXISTS `findavet` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS `findavet`;
 USE `findavet`;
 
--- Dumping structure for table findavet.users
 CREATE TABLE IF NOT EXISTS `users` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -25,11 +9,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table findavet.users: ~0 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
--- Dumping structure for table findavet.vets
 CREATE TABLE IF NOT EXISTS `vets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -38,11 +17,3 @@ CREATE TABLE IF NOT EXISTS `vets` (
   `phone` varchar(13) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table findavet.vets: ~0 rows (approximately)
-/*!40000 ALTER TABLE `vets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vets` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
