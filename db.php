d3e6bcb066052cd1fef1aba5d94100cb685140f6
MichaelRP1 <22685648+MichaelRP1@users.noreply.github.com>
2019-08-16 19:15:38 -0400

    Updating Database Values for Website Hosting (CPanel/NameCheap)

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php // Configuration to Set Database Variables
 define('DB_SERVER', 'localhost'); // Sets the Server to the Local Server
 define('DB_PORT', '3306'); // Default MySQL Port
-define('DB_USER', 'root'); // Default MySQL SuperUser Account
-define('DB_PASS', ''); // Default Password for root since it can only be accessed from localhost
-define('DB_DB', 'findavet'); // Name of Database on Server
+define('DB_USER', 'cronhxtd_findavet'); // Default MySQL SuperUser Account
+define('DB_PASS', '2AR88VMEfe4Y'); // Default Password for root since it can only be accessed from localhost
+define('DB_DB', 'cronhxtd_findavet'); // Name of Database on Server
 
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_DB, DB_PORT); // Connects to MySQL Server
 ?>
